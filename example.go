392684a4eb57f6cc8a764ed51ebb10e0cd6266ce
MSCxar0293 <mscxprivatemail@163.com>
2022-03-24 09:40:51 +0800

    available

--- example.go
+++ example.go
@@ -1,39 +1,27 @@
 //Package example
 package example
 
-import (
-	"math/rand"
-	"strconv"
-	"strings"
+import ( 
+	control "github.com/FloatTech/zbputils/control"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/message"
 
-	control "guthb.com/FloatTech/zbputils/control"
-	zero "github.com/wdvxdr1123/Zerobot/message"
-
-	"github.com/FloatTech/Zerobot-Plugin/order"
+	"github.com/FloatTech/zbputils/control/order"
 )
 
 func init(){
-	engine := control.Register("example",order.PrioChoose, &control.Options{
+	engine := control.Register("example",order.AcquirePrio(), &control.Options{
 		DisableOnDefault: false,
 		Help: "example\n" +
-		"- hello, world! "
-		"- 使用test以验证你的第一个插件。"
+		"- hello, world! \n" +
+		"- 使用test以验证你的第一个插件。",
 	})
 	engine.OnFullMatch ("test").SetBlock(true).
-	Handle(func(*zero.Ctx){
+	Handle(func(ctx *zero.Ctx){
 		ctx.SendChain(message.Text("hello, world! "))
 	})
 	engine.OnFullMatch ("hello, world!").SetBlock(true).
-	Handle(func(*zero.Ctx){
+	Handle(func(ctx *zero.Ctx){
 		ctx.SendChain(message.Text("成功！"))
 	})
-	initChatList(func(){
-		engine.OnFullMatchGroup(chatList, zero.OnlyToMe).SetBlock(true).Handle(
-			func(ctx *zero.Ctx){
-				key:=ctx.MessageString()
-				val := *kimomap[key]
-				text:=val[rand.Intn(len(val))]
-				ctx.SendChain(message.Reply(ctx.Event.MessageID), message.Text(text))
-			})
-		})
 }
